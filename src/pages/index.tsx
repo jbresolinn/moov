59c767849f30c77dfede155dde0b3c001dfae5a1
juliabresolin <juliabresolin2@gmail.com>
2021-02-26 22:59:44 -0300

    layout: create homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,58 +1,28 @@
-import { CompletedChallenges } from "../components/CompletedChallenges";
-import { Countdown } from "../components/Countdown";
-import { ExperienceBar } from "../components/ExperienceBar";
-import { Profile } from "../components/Profile";
 import Head from 'next/head';
 
 import styles from '../styles/pages/Home.module.css'
-import { ChallengeBox } from "../components/ChallengeBox";
-import { CountdownProvider } from "../contexts/CountdownContext";
-import { GetServerSideProps } from "next";
-import { ChallengesProvider } from "../contexts/ChallengesContext";
 
-interface HomeProps {
-  level: number;
-  currentExperience: number;
-  challengesCompleted: number;
-}
-
-export default function Home(props: HomeProps) {
+export default function Home() {
   return (
-    <ChallengesProvider 
-    level={props.level} 
-    currentExperience={props.currentExperience} 
-    challengesCompleted={props.challengesCompleted}>
     <div className={styles.container}>
-      <Head>
-        <title>home | moov</title>
-      </Head>
+      <img src="/symbol-home.svg" alt="moov symbol"/>
 
-      <ExperienceBar />
+      <div className={styles.content}>
+      <header>
+        <img src="/logo-full-white.svg" alt="moov logo"/>
+        <p>A combination of the pomodoro technique with simple exercises that will improve your quality of life.</p>
+      </header>
 
-      <CountdownProvider>
-        <section>
-          <div>
-            <Profile />
-            <CompletedChallenges />
-            <Countdown />
-          </div>
-          <div>
-            <ChallengeBox />
-          </div>
-        </section>
-      </CountdownProvider>
+      <span>Login to continue</span>
+      <button type="button" className={styles.githubLogin}>
+        <img src="/icons/github.svg" alt="github icon"/>
+        Login with Github
+      </button>
+      <button type="button" className={styles.googleLogin}>
+        <img src="/icons/google.svg" alt="google icon"/>
+        Login with Google
+      </button>
+      </div>
     </div>
-    </ChallengesProvider>
   )
-}
-
-export const getServerSideProps: GetServerSideProps = async (ctx) => {
-  const {level, currentExperience, challengesCompleted} = ctx.req.cookies;
-  return {
-    props: {
-      level: Number(level),
-      currentExperience: Number(currentExperience),
-      challengesCompleted: Number(challengesCompleted)
-    }
-  }
-}
+} 
